3fb304796564d3d5b43b18b2d25c729ffb0b36d4
AyanKhurt <ayankhurt@gmail.com>
2024-08-21 23:05:44 -0700

    new

---
+++ script.js
@@ -0,0 +1,36 @@
+let firstAlert =("Error! Please enter a valid password");
+alert(firstAlert);
+let secondAlert =("Welcome to JS land \n Happy Coding!");
+alert(secondAlert);
+let thirdAlert =("Welcome to JS Land...");
+alert(thirdAlert);
+let fourAlert =("Happy Coding! \n Prevent this page from creating additonal dialogs");
+alert(fourAlert);
+let fiveAlert =("Hello... I can run JS through my web browser's console");
+alert(fiveAlert);
+
+console.log(fiveAlert)
+
+// let username =("");
+// alert(username);
+let studentNo1 = {
+    userName : "AyanKhurt",
+    myName : "Ayan Khurram",
+    message : "Hello World",
+}
+
+
+let studNo1UserName = ("AyanKhurt");
+let studNo1myName = ("Ayan Khurram");
+let studNo1message = ("Hello World");
+console.log (studentNo1)
+alert(studNo1UserName);
+alert(studNo1myName);
+alert(studNo1message);
+
+let pizza = ("PIZZA \n PIZZ \n PIZ \n PI \n P")
+alert(pizza)
+let email = ("ayankhurt@gmail.com");
+alert(email)
+let book = ("A smarter way to learn JavaScript");
+alert(book)
